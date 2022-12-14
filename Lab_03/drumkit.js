f2227cc92af3cedc78b8f99b4b521276cf1469e7
k-wos <kacper.wos33@gmail.com>
2022-12-15 00:41:20 +0100

    drumkit panda3

--- Lab_03/drumkit.js
+++ Lab_03/drumkit.js
@@ -1,35 +1,85 @@
-document.addEventListener('keypress', onKeyPress)
 
-function onKeyPress(event){
-    const key = event.key
-    let sound = ''
-    switch(key){
-        case 'q':
-            sound = 'clap'
-            break
-        case 'w':
-            sound = 'kick'
-            break
-        case 'e':
-            sound = 'boom'
-            break
-        case 'r':
-            sound = 'snare'
-            break
+const sounds = {
+    'q': './sounds/clap.wav',
+    'w':'./sounds/boom.wav',
+    'e':'./sounds/kick.wav',
+    'r':'./sounds/snare.wav'
+};
+
+
+
+const channels =[
+    [],
+    [],
+    [],
+    []
+];
+let isRecording = false;
+let currentChannel = 0;
+
+document.addEventListener('keypress', event=>{
+    if(sounds.hasOwnProperty(event.key)){
+        const audioElement = new Audio(sounds[event.key]);
+        audioElement.play();
+
+        if(isRecording){
+            channels[currentChannel].push(event.key);
+        }
     }
-    
-    
-    
-    playSound(sound)
-    console.log(key)
+})
+
+
+document.querySelector('#recordButton').addEventListener('click',() =>{
+    isRecording = true;
+    console.log(isRecording)
+});
+
+document.querySelector('#stopButton').addEventListener('click',() =>{
+    isRecording = false;
+    currentChannel = (currentChannel + 1) % 4;
+    console.log(isRecording)
+});
+
+document.querySelector('#playButton').addEventListener('click',()=>{
+    for(let i=0; i<4; i++){
+        const channel = channels[i];
+        for(let j=0; j<channel.length; j++){
+            setTimeout(()=>{
+                const audioElement = new Audio(sounds[channel[j]]);
+                audioElement.play();
+                console.log(channel[j])
+            }, j*1000);
+        }
+    }
+})
+
+function playChannel(channelNumber){
+    const channel = channels[channelNumber];
+    for(let i = 0; i< channel.length; i++){
+        setTimeout(()=>{
+            const audioElement = new Audio(sounds[channel[i]]);
+            audioElement.play();
+        }, i*1000);
+    }
 }
 
-function playSound(sound){
-    const audioTag = document.querySelector('#'+ sound)
-    audioTag.currentTime = 0
-    audioTag.play()
-   
-}
-var song = [];
+document.querySelector('#channel1Button').addEventListener('click', ()=>{
+    playChannel(0);
+})
+document.querySelector('#channel2Button').addEventListener('click', ()=>{
+    playChannel(1);
+})
+document.querySelector('#channel3Button').addEventListener('click', ()=>{
+    playChannel(2);
+})
+document.querySelector('#channel4Button').addEventListener('click', ()=>{
+    playChannel(3);
+})
+document.querySelector('#allChannels').addEventListener('click', ()=>{
+   for(let i =0;i<4;i++){
+    playChannel(i);
+   }
+})
 
-function startRecording()
+
+
