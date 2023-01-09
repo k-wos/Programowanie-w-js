0987b511652ebaf2fe9f2a7594f729fbd95b341e
k-wos <kacper.wos33@gmail.com>
2023-01-09 21:27:10 +0100

    slider update

--- Lab_02/main.js
+++ Lab_02/main.js
@@ -1,14 +1,54 @@
-let index = 0;
-showSlides();
+let index = 1;
+let slides = document.querySelectorAll('.img');
+let dots = document.querySelectorAll('.dot');
 
-function showSlides(){
+/*function showSlides(){
     let i;
-    let slides = document.getElementsByClassName("img");
     for(i=0;i<slides.length;i++){
         slides[i].style.display = "none";
     }
     index ++;
     if(index >slides.length){index =1}
     slides[index-1].style.display = "block";
+    
     setTimeout(showSlides,2000);
 }
+
+showSlides();*/
+
+const updateSlider = () => {
+    for(let i =0; i< slides.length; i++){
+        slides[i].style.display ='none';
+       
+    }
+    slides[index-1].style.display ='block';
+   
+    for (let i=0; i< dots.length; i++){
+        dots[i].style.backgroundColor = 'gray';
+    }
+    dots[index-1].style.backgroundColor = 'black';
+
+    if(index === slides.length){
+        index = 1;
+    }
+    else{
+        index ++;
+    }
+    
+}
+
+document.querySelector('.previous').addEventListener('click', () => {
+    index--;
+    updateSlider();
+});
+document.querySelector('.next').addEventListener('click', () => {
+    index++;
+    updateSlider();
+});
+setInterval(updateSlider,5000);
+
+
+
+
+
+
