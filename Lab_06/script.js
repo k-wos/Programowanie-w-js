5e3acd58002d40cf349dfe677337d323bf05027c
k-wos <80058894+k-wos@users.noreply.github.com>
2023-01-18 18:56:23 +0100

    ball in the hole

--- Lab_06/script.js
+++ Lab_06/script.js
@@ -1,30 +1,55 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-let ball = {x:150, y: 150, radius: 10, velX: 5, velY:5};
-let hole = {x:250, y: 250, radius: 20};
+let x =50;
+let y=50;
+const radius = 20;
+const holeX = 200;
+const holeY = 200;
+const holeRadius = 30;
 
-const maxX = canvas.clientWidth - ball.clientWidth;
-const maxY = canvas.clientHeight - ball.clientHeight;
+window.addEventListener('deviceorientation', handleOrientation);
 
-window.addEventListener("deviceorientation", DeviceMove);
+function handleOrientation(event){
+    x+=event.gamma/30;
+    y-=event.beta/30;
 
-function DeviceMove(event){
-    ball.velX = event.gamma / 90;
-    ball.velY = event.beta / 90;
+    if (x - radius < 0) {
+        x = radius;
+      }
+      if (x + radius > canvas.width) {
+        x = canvas.width - radius;
+      }
+      if (y - radius < 0) {
+        y = radius;
+      }
+      if (y + radius > canvas.height) {
+        y = canvas.height - radius;
+      }
+    checkforCollision();
+    draw();
 }
 
-function drawBallAndHole(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.arc(hole.x,hole.y,hole.radius,0,2*Math.PI);
-    ctx.stroke();
-    ball.x += ball.velX;
-    ball.y += ball.velY;
-    requestAnimationFrame(drawBallAndHole);
-    
+function checkforCollision(){
+    const distance = Math.sqrt(Math.pow(x-holeX,2)+Math.pow(y-holeY,2));
+
+    if(distance<radius+holeRadius){
+        alert("Wygrałeś");
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        
+    }
 }
-drawBallAndHole();
+
+    function draw(){
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(x,y,radius,0,2*Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(holeX, holeY, holeRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = "green";
+        ctx.fill();
+        ctx.closePath();
+    }
