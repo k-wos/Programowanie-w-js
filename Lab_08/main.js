ea6ecdbf0ba99820e8c681277e1a85a723ee3024
k-wos <80058894+k-wos@users.noreply.github.com>
2023-01-18 23:04:57 +0100

    canvas

--- Lab_08/main.js
+++ Lab_08/main.js
@@ -1,97 +1,69 @@
-const canvas = document.querySelector('#myCanvas');
-const ctx = canvas.getContext('2d');
-const canvasWidhth = canvas.width;
-const canvasHeight = canvas.height;
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+let X = document.getElementById("xValue");
+let numOfBalls;
+let dis = document.getElementById("yValue");
+let distanceValue;
 
-const rnd = (max, min) => {
-    if (min)
-        return Math.random() * (max - min) + min;
-    else
-        return Math.random() * max;
+const startButton = document.querySelector("#start-button");
+
+let circles = [];
+
+
+
+startButton.addEventListener("click", begin);
+
+ function begin(){
+     numOfBalls = parseInt(X.value);
+     distanceValue = parseInt(dis.value)
+
+
+for (let i = 0; i<numOfBalls; i++ ){
+    const circle ={
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        directionX: Math.random() *2-1,
+        directionY: Math.random() *2-1,
+        speed: Math.random() * 3+1
+    };
+    circles.push(circle)
 }
+animate();
+console.log(Y)
+};
 
-const map = (value, start1, stop1, start2, stop2, withinBounds = false) => {
-    const newVal = (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-    if(!withinBounds)
-        return newVal;
-    if(start2 < stop2)
-        return constrain(newVal, start2, stop2);
-    else
-        return constrain(newVal, stop2, start2);
-}
+function animate( ){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
 
- class Ball {
-    constructor() {
-        this.xPosition = rnd(canvasWidhth - 10, 10);
-        this.yPosition = rnd(canvasHeight - 10, 10);
-        this.radius = 10;
-        this.xSpeed = rnd(1, -1) / 10;
-        this.ySpeed = rnd(1, -1) / 10;
-    }
-
-    moveBall = () => {
-        this.xPosition += this.xSpeed;
-        this.yPosition += this.ySpeed;
-    }
-
-    checkEdges = () => {
-        if (this.xPosition > canvasWidhth - 10 || this.xPosition - 10 < 0)
-            this.xSpeed = this.xSpeed * (-1);
-        if (this.yPosition > canvasHeight - 10 || this.yPosition - 10 < 0)
-            this.ySpeed = this.ySpeed * (-1);
-    }
-
-    drawBall = () => {
+    for(let i =0; i<numOfBalls;i++){
+        const circle = circles[i];
+        ctx.fillStyle = "#0000ff";
         ctx.beginPath();
-        ctx.strokeStyle = 'rgb(118, 200, 66)';
-        const startAngle = 0;
-        const endAngle = 2 * Math.PI;
-        ctx.arc(this.xPosition, this.yPosition, this.radius, startAngle, endAngle);
-        ctx.stroke();
-    }
+        ctx.arc(circle.x, circle.y, 10, 0, Math.PI * 2);
+        ctx.fill();
 
-    connectBall = (otherBall) => {
-        const x1 = this.xPosition
-        const y1 = this.yPosition
-        const x2 = otherBall.xPosition
-        const y2 = otherBall.yPosition
-        const a = x2 - x1;
-        const b = y2 - y1;
-        const c = Math.sqrt(a ** 2 + b ** 2);
+        circle.x += circle.directionX * circle.speed;
+        circle.y += circle.directionY * circle.speed;
 
-        if (c <= canvasWidhth * 0.2) {
-            const lineSize = map(c, 0, canvasWidhth * 0.2, 1, false) //FIXME: here too
-            ctx.beginPath();
-            ctx.moveTo(x1, y1);
-            ctx.strokeStyle = `rgba(101, 130, 76, ${lineSize})`;
-            ctx.lineTo(x2, y2);
-            ctx.stroke();
+        if(circle.x > canvas.width || circle.x < 0){
+            circle.directionX = -circle.directionX;
         }
-    }
-}
-const generateBalls = (number) => {
-    const array = [];
-    for (let i = 0; i < number; i++) {
-        const newBall = new Ball();
-        array.push(newBall);
-    }
-    return array;
-}
-
-let ballArray = generateBalls(30)
-const draw = () => {
-    ctx.clearRect(0, 0, canvasWidhth, canvasHeight);
-
-    for (let i = 0; i < ballArray.length; i++) {
-        for (let j = 1; j < ballArray.length; j++) {
-            ballArray[i].drawBall();
-            ballArray[i].moveBall();
-            ballArray[i].checkEdges();
-            if (ballArray[i] !== ballArray[j])
-                ballArray[j].connectBall(ballArray[i]);
+        if(circle.y > canvas.height || circle.y < 0){
+            circle.directionY = -circle.directionY;
         }
-    }
 
-    stopDraw = requestAnimationFrame(draw);
+        for(let j = i + 1; j<numOfBalls; j++){
+            const circleToCompare = circles[j];
+            const distance = Math.sqrt(Math.pow(circle.x - circleToCompare.x,2) + Math.pow(circle.y - circleToCompare.y,2));
+            if(distance < distanceValue){
+                ctx.strokeStyle = "#000"; 
+                ctx.beginPath();
+                ctx.moveTo(circle.x, circle.y);
+                ctx.lineTo(circleToCompare.x, circleToCompare.y);
+                ctx.stroke();
+            }
+        }
+        
+    }
+    requestAnimationFrame(animate);
 }
-draw();
