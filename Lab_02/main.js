448f6bd93d33c6178accbe2942a7a6c8bbbb96a1
k-wos <80058894+k-wos@users.noreply.github.com>
2023-01-18 17:03:16 +0100

    slider

--- Lab_02/main.js
+++ Lab_02/main.js
@@ -1,16 +1,36 @@
-var index = 1;
+let index = 1;
+const slides = document.getElementsByClassName("photo");
+const dots = document.getElementsByClassName("dots")[0];
+let dot;
+
+for(let i =0; i < slides.length; i++){
+    dot = document.createElement("li");
+    dot.setAttribute("class", "dot");
+    dot.addEventListener("click", function(){
+    index = i+1;
+    showSlides(index);
+    });
+    dots.appendChild(dot);
+}
+
 showSlides(index);
 
 document.querySelector('.previous').addEventListener('click', function(){
+    clearInterval(slideInterval);
     showSlides(index-=1);
 });
 
 document.querySelector('.next').addEventListener('click', function(){
+    clearInterval(slideInterval);
     showSlides(index+=1);
 });
 
+const slideInterval = setInterval(function(){
+    showSlides(index +=1);
+},5000);
+
 function showSlides(n){
-    const slides = document.getElementsByClassName("photo");
+    
 
     if(n > slides.length){
        index = 1;
@@ -22,7 +42,11 @@ function showSlides(n){
 
     for(let i = 0; i<slides.length ;i++){
         slides[i].style.display = "none";
+        dots.childNodes[i].classList.remove("active");
     }
 
     slides[index - 1].style.display="block";
+    dots.childNodes[index - 1].classList.add("active");
 }
+
+
